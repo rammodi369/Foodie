ad2899274ec2de570dcd8d4879b15a50f0bd4bf0
Ram Modi <112546756+rammodi369@users.noreply.github.com>
2023-08-16 16:51:54 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
  const navigate=useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginUser", {
+    const response = await fetch("https://foodie2.onrender.com/api/loginUser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
