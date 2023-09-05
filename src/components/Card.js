7800906cbbc796439c8885b045582bc47ea0514a
Ram Modi <112546756+rammodi369@users.noreply.github.com>
2023-09-05 21:55:49 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -39,7 +39,7 @@ function Card(props) {
         setSize(priceRef.current.value);
     }, [])
     return (
-        <div>
+        <div className="mx-auto">
             <div class="card mt-3" style={{ width: "18rem", maxWeight: "160px" }}>
                 <img
                     src={props.foodItem.img}
