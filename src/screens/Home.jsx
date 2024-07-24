601ff10e9d07e1da07dec1fe60311083711bc4e3
Ram Modi <112546756+rammodi369@users.noreply.github.com>
2024-07-24 23:25:06 +0530

    Update image links

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100 yo"
-          src="https://source.unsplash.com/random/900x700/?burger"
+          src="https://static.wixstatic.com/media/2cbff6_cf6583ae65b346e7a22a5ecacf74453f~mv2.jpg/v1/fill/w_900,h_700,al_c,q_85/2cbff6_cf6583ae65b346e7a22a5ecacf74453f~mv2.jpg"
           alt="First slide"
           style={{filter:"brightness(30%)"}}
         />
@@ -54,7 +54,7 @@ function Home() {
       <Carousel.Item interval={500}>
         <img
           className="d-block w-100 yo"
-          src="https://source.unsplash.com/random/900x700/?pastry"
+          src="https://static.wixstatic.com/media/2cbff6_cf6583ae65b346e7a22a5ecacf74453f~mv2.jpg/v1/fill/w_900,h_700,al_c,q_85/2cbff6_cf6583ae65b346e7a22a5ecacf74453f~mv2.jpg"
           alt="Second slide"
           style={{filter:"brightness(30%)"}}
         />
@@ -74,7 +74,7 @@ function Home() {
       <Carousel.Item>
         <img
           className="d-block w-100 yo"
-          src="https://source.unsplash.com/random/900x700/?barbeque"
+          src="https://static.wixstatic.com/media/2cbff6_cf6583ae65b346e7a22a5ecacf74453f~mv2.jpg/v1/fill/w_900,h_700,al_c,q_85/2cbff6_cf6583ae65b346e7a22a5ecacf74453f~mv2.jpg"
           alt="Third slide"
           style={{filter:"brightness(30%)"}}
         />
